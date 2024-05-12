ab5d849ef993c0390fc22b7cc373173d79f75e2b
AnelCC <anel.calvo.cervantes@gmail.com>
2024-05-12 09:23:11 -0500

    Add action to main screen

--- app/src/main/java/com/anelcc/widgetpinning/MainActivity.kt
+++ app/src/main/java/com/anelcc/widgetpinning/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.anelcc.widgetpinning
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +22,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.anelcc.widgetpinning.ui.theme.WidgetPinningTheme
+import com.anelcc.widgetpinning.widget.SampleWidgetReceiver
+import com.anelcc.widgetpinning.widget.WidgetPinnedReceiver
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,8 +57,27 @@ class MainActivity : ComponentActivity() {
         )
     }
 
-
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun callWidgetPinner(context: Context) {
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        val widgetProvider = ComponentName(context, SampleWidgetReceiver::class.java)
 
+        val pinnedIntent = Intent(context, WidgetPinnedReceiver::class.java)
+        pinnedIntent.putExtra("requestCode", 7)
+
+        if (appWidgetManager.isRequestPinAppWidgetSupported) {
+            // Create the PendingIntent object only if your app needs to be notified
+            // when the user chooses to pin the widget. Note that if the pinning
+            // operation fails, your app isn't notified. This callback receives the ID
+            // of the newly pinned widget (EXTRA_APPWIDGET_ID).
+            val successCallback = PendingIntent.getBroadcast(
+                context,
+                0,
+                pinnedIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+            )
+
+            appWidgetManager.requestPinAppWidget(widgetProvider, null, successCallback)
+        }
     }
 }
